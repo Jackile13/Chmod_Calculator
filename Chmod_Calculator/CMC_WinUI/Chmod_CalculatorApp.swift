c907ad07dfca4ddb45c884b475d722e0bea47fa4
Jack A <82874057+Jackile13@users.noreply.github.com>
2024-05-11 17:21:49 +1000

    init

---
+++ Chmod_Calculator/CMC_WinUI/Chmod_CalculatorApp.swift
@@ -0,0 +1,46 @@
+import Foundation
+import UWP
+import WinAppSDK
+import WindowsFoundation
+import WinUI
+
+@main
+public class PreviewApp: SwiftApplication {
+    /// A required initializer for the application. Non-UI setup for your application can be done here.
+    /// Subscribing to unhandledException is a good place to handle any unhandled exceptions that may occur
+    /// in your application.
+    public required init() {
+        super.init()
+        unhandledException.addHandler { (_, args:UnhandledExceptionEventArgs!) in
+            print("Unhandled exception: \(args.message)")
+        }
+    }
+
+    /// onShutdown is called once Application.start returns. This is a good place to do any cleanup
+    /// that is necessary for your application before it terminates.
+    override public func onShutdown() {    }
+
+    /// onLaunched is called when the application is launched. 
+    /// This is where the window is configured and where the initial UI can be created
+    override public func onLaunched(_ args: WinUI.LaunchActivatedEventArgs) {
+        let window = Window()
+        window.title = "WinUI3AnimationsPreview_EDITED"
+
+        try! window.activate()
+
+        let btn = Button()
+        btn.content = "A test button"
+        btn.click.addHandler { _, args in
+            print("Button Clicked!")
+            print(args.debugDescription)
+        }
+
+        let panel = StackPanel()
+        panel.orientation = .vertical
+        panel.spacing = 10
+        panel.horizontalAlignment = .center
+        panel.verticalAlignment = .center
+        panel.children.append(btn)
+        window.content = panel
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let GUILinkerSettings: [LinkerSetting] = [
+    .unsafeFlags(["-Xlinker", "/SUBSYSTEM:WINDOWS"], .when(configuration: .release)),
+    // Update the entry point to point to the generated swift function, this lets us keep the same main method
+    // for debug/release
+    .unsafeFlags(["-Xlinker", "/ENTRY:mainCRTStartup"], .when(configuration: .release)),
+]
+
+let package = Package(
+    name: "Chmod Calculator",
+    products: [
+        .executable(name: "Chmod_Calculator", targets: ["Chmod_Calculator"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/thebrowsercompany/swift-windowsappsdk", branch: "main"),
+        .package(url: "https://github.com/thebrowsercompany/swift-windowsfoundation", branch: "main"),
+        .package(url: "https://github.com/thebrowsercompany/swift-winui", branch: "main"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "Chmod_Calculator",
+            dependencies: [
+                .product(name: "WinUI", package: "swift-winui"),
+                .product(name: "WinAppSDK", package: "swift-windowsappsdk"),
+                .product(name: "WindowsFoundation", package: "swift-windowsfoundation"),
+            ],
+            path:"Chmod_Calculator",
+            exclude: ["CMC_SwiftUI"],
+            linkerSettings: GUILinkerSettings
+        ),
+    ]
+)
